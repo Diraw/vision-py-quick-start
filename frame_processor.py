39f0b0789ff9b8efedae1f1c058dabccaa06a6ef
diraw <craftyup7567@outlook.com>
2024-08-18 13:51:59 +0800

    update frame_processor.py

--- frame_processor.py
+++ frame_processor.py
@@ -24,7 +24,7 @@ def process_frame(frame, pTime):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 # print(id, cx, cy)
-                cv2.circle(img, (cx, cy), 15, (255, 0, 255), 1)
+                # cv2.circle(img, (cx, cy), 15, (255, 0, 255), 1)
 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
